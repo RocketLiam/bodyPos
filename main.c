1b4cf94f15c0eea14d0fbe319c7dcd3ee99371b2
Liam Harkness <rocketliam@Wisp.local>
2024-05-13 14:53:09 -0400

    enum

--- main.c
+++ main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 
+char text[30];
+
+enum gatesNames {
+    awesome
+} gate;
+
 int main(int argc, char const *argv[])
 {
     printf("You find your self sourneded by many gates, the gates read\n1.\n2.\n3.\n");
+    fgets(text, sizeof(text), stdin);
+    printf("You have stated %s", text);
     return 0;
+}
+
+void gate1() {
+
+}
+
+void gate2() {
+    
+}
+
+void gate3() {
+    
+}
+
+void gate4() {
+    
+}
+
+void gate5() {
+    
+}
+
+void gate6() {
+    
+}
+
+void gate7() {
+    
+}
+
+void gate8() {
+    
 }
